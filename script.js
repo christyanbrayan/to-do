b2bc9db2582aeeb1dda9a25c87d191671748cd74
christyanbrayan <christyankev@gmail.com>
2020-09-03 18:00:49 -0300

    update PR #1 changes

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
-var listElement = document.querySelector('#app ul');
-var inputElement = document.querySelector('#app input');
-var buttonElement = document.querySelector('#app button');
+const listElement = document.querySelector("#app ul");
+const inputElement = document.querySelector("#app input");
+const buttonElement = document.querySelector("#app button");
 
-var toDos = JSON.parse(localStorage.getItem('list_toDos')) || [];
+const toDos = JSON.parse(localStorage.getItem("list_toDos")) || [];
 
 function renderToDos() {
-	listElement.innerHTML = '';
-	
-	for (toDo of toDos) {
-		var toDoElement = document.createElement('li');
-		var toDoText = document.createTextNode(toDo);
-		
-		var linkElement = document.createElement('a');
-		
-		linkElement.setAttribute('href', '#');
-		
-		var pos = toDos.indexOf(toDo);
-		linkElement.setAttribute('onclick', 'deleteToDo(' + pos + ')');
-		
-		var linkText = document.createTextNode(' Delete');
+  listElement.innerHTML = "";
 
-		linkElement.appendChild(linkText);
-		
-		toDoElement.appendChild(toDoText);
-		toDoElement.appendChild(linkElement);
-		
-		listElement.appendChild(toDoElement);
-	}
+  for (toDo of toDos) {
+    let toDoElement = document.createElement("li");
+    let toDoText = document.createTextNode(toDo);
+
+    let linkElement = document.createElement("a");
+
+    linkElement.setAttribute("href", "#");
+
+    let pos = toDos.indexOf(toDo);
+    linkElement.setAttribute("onclick", "deleteToDo(" + pos + ")");
+
+    let linkText = document.createTextNode(" Delete");
+
+    linkElement.appendChild(linkText);
+
+    toDoElement.appendChild(toDoText);
+    toDoElement.appendChild(linkElement);
+
+    listElement.appendChild(toDoElement);
+  }
 }
 
 renderToDos();
 
 function addToDo() {
-	var toDoText = inputElement.value;
-	
-	toDos.push(toDoText);
-	inputElement.value = '';
-	renderToDos();
-	saveToStorage();
+  const toDoText = inputElement.value;
 
-	inputElement.value = '';
-	inputElement.focus();
+  toDos.push(toDoText);
+  inputElement.value = "";
+  renderToDos();
+  saveToStorage();
+
+  inputElement.value = "";
+  inputElement.focus();
 }
 
 buttonElement.onclick = addToDo;
 
 function deleteToDo(pos) {
-	toDos.splice(pos, 1);
-	renderToDos();
-	saveToStorage();
+  toDos.splice(pos, 1);
+  renderToDos();
+  saveToStorage();
 }
 
 function saveToStorage() {
-	localStorage.setItem('list_toDos', JSON.stringify(toDos));
+  localStorage.setItem("list_toDos", JSON.stringify(toDos));
 }
